c67b9b67971636330edbae4654d2bdb46fa67d0e
beauxvice <gregybos@gmail.com>
2022-01-08 02:14:14 -0600

    more init

---
+++ webpack.config.ts
@@ -0,0 +1,65 @@
+import path from 'path';
+import { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+const webpack = require('webpack')
+
+const distDir = path.resolve(__dirname, 'client/dist');
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+export const config: Configuration = {
+  mode: 'development',
+  watch: true,
+  entry: './client/src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+			{
+				test: /\.css$/,
+				use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }]},
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json', '.jsx', '.css', 'scss'],
+  },
+  output: {
+    path: distDir,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: distDir,
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.join(__dirname, 'client/src', 'index.html') }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'CLIENT_ID':JSON.stringify('process.env.CLIENT_ID'),
+        'CLIENT_SECRET': JSON.stringify('process.env.CLIENT_SECRET')
+      }
+   }),
+  ],
+};
+
+export default config;
